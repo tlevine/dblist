441804772282d54d217b09701a6b2818b443eecb
Thomas Levine <thomas.levine@gmail.com>
2012-01-02 10:04:01 -0500

    outline

--- dblist.py
+++ dblist.py
@@ -2,6 +2,7 @@
 #http://en.wikibooks.org/wiki/Python_Programming/Classes
 
 class dblist:
+  #List methods
   def append(self):
   def count(self):
   def extend(self):
@@ -11,4 +12,20 @@ class dblist:
   def remove(self):
   def reverse(self):
   def sort(self):
+
+  #Special list methods
+  def __init__(self):
+  def __del__(self):
+  def __str__(self):
+  def __unicode__(self):
+
+  #Item (index) operators
+  def __getitem__(self,index):
+    return
+  def __setitem__(self,index):
+    return
+  def __getslice__(self,index):
+    return
+
+  #Non-list methods
   def commit(self): 
---
+++ tests.py
@@ -0,0 +1,9 @@
+import unittest
+from dblist import dblist
+
+class TestDbList(unittest.TestCase):
+  def test_append(self):
+
+
+if __name__ == '__main__':
+  unittest.main()
