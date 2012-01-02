57585f5b064924f931a0d26c87d6fcc557729d6b
Thomas Levine <thomas.levine@gmail.com>
2012-01-02 09:56:25 -0500

    list functions

--- dblist.py
+++ dblist.py
@@ -2,4 +2,13 @@
 #http://en.wikibooks.org/wiki/Python_Programming/Classes
 
 class dblist:
-  
+  def append(self):
+  def count(self):
+  def extend(self):
+  def index(self):
+  def insert(self):
+  def pop(self):
+  def remove(self):
+  def reverse(self):
+  def sort(self):
+  def commit(self): 
